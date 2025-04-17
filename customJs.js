5f59378a20ce4b7685832d9d98cf70ef24576779
Dev-Dolawatta <.dtdolawatta@gmail.com>
2025-04-17 13:01:04 +0530

    make the header responsive

--- customJs.js
+++ customJs.js
@@ -1,6 +1,10 @@
-$(document).ready(function (){
-    $("#menu").click(function (){
-        $(this).toggleClass("fa-times");
-        $("header").toggleClass("toggle");
+$(document).ready(function(){
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('header').toggleClass('toggle');
+    });
+    $(window).on('scroll load',function(){
+        $('#menu').removeClass('fa-times');
+        $('header').removeClass('toggle');
     });
 });
